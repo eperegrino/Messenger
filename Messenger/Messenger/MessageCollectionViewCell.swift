e01ae1f3526fffa6ba0fd3010c0b700f279544d4
eperegrino <eduardo.peregrino@gmail.com>
2021-10-13 13:54:35 -0300

    Layout changes and fixes

--- Messenger/Messenger/MessageCollectionViewCell.swift
+++ Messenger/Messenger/MessageCollectionViewCell.swift
@@ -10,17 +10,33 @@ import UIKit
 class MessageCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Views -
+    lazy var leftImageBackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray5
+        view.layer.cornerRadius = view.frame.width/2
+        
+        return view
+    }()
+    
     let nameLabel: UILabel = {
         let label = UILabel()
         label.font = label.font.withSize(14)
         
         return label
     }()
+    
     let messageLabel: UILabel = {
         let label = UILabel()
         label.font = label.font.withSize(12)
         
         return label
+    }()
+    
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray4
+        
+        return view
     }()
     
     // MARK: - Init -
@@ -42,19 +58,35 @@ class MessageCollectionViewCell: UICollectionViewCell {
 // MARK: - ViewCode -
 extension MessageCollectionViewCell: ViewCode {
     func setupSubviews() {
+        addSubview(leftImageBackground)
         addSubview(nameLabel)
         addSubview(messageLabel)
+        addSubview(separatorView)
     }
     
     func setupConstraints() {
-        nameLabel.layout {
-            $0.top == top
+        leftImageBackground.layout {
             $0.leading == leading + 20
+            $0.centerY == centerY
+            $0.width == 40
+            $0.height == 40
+        }
+        
+        nameLabel.layout {
+            $0.top == leftImageBackground.top + 2
+            $0.leading == leftImageBackground.trailing + 5
         }
         
         messageLabel.layout {
-            $0.top == nameLabel.bottom + 10
-            $0.leading == leading + 20
+            $0.top == leftImageBackground.centerY
+            $0.leading == leftImageBackground.trailing + 5
+        }
+        
+        separatorView.layout {
+            $0.leading == leading + 10
+            $0.bottom == bottom
+            $0.trailing == trailing - 10
+            $0.height == 1
         }
     }
 }
